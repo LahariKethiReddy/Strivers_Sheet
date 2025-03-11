c6c321342e08fe17d28e41fc559682949372ed37
LahariKethiReddy <laharikethireddy88@gmail.com>
2025-03-11 21:00:21 +0530

    Add files via upload

---
+++ linkedlist-bestcase.py
@@ -0,0 +1,10 @@
+class Solution:
+    def constructLL(self, arr):
+        if not arr:
+            return None
+        head = Node(arr[0])  
+        current = head
+        for value in arr[1:]:  
+            current.next = Node(value)
+            current = current.next 
+        return head
